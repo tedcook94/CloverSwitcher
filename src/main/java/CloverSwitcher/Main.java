67540eb7853b14c3d24350c5e9fec73349d755e1
tedcook94 <33679293+tedcook94@users.noreply.github.com>
2018-09-27 10:21:53 -0500

    Adds functionality that closes all windows if main window is closed

--- src/main/java/CloverSwitcher/Main.java
+++ src/main/java/CloverSwitcher/Main.java
@@ -1,6 +1,7 @@
 package CloverSwitcher;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,6 +14,7 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("mainWindow.fxml"));
         primaryStage.setTitle("Clover Switcher");
         primaryStage.setScene(new Scene(root, 725, 600));
+        primaryStage.setOnHidden(e -> Platform.exit());
         primaryStage.show();
     }
 
